67d0bf2266727a68f66ff567518bdeab532a9934
nate zhou <gnuunixchad@outlook.com>
2026-03-03 13:40:25 +0800

    Mod(update.sh): include udev rules for battery

--- update.sh
+++ update.sh
@@ -7,7 +7,11 @@ dotfiles="${HOME}/doc/unixchad/dotfiles"
 damblocks="${HOME}/doc/unixchad/damblocks"
 
 scripts="audio bright calcurse-num-cron checkupdates-cron dam-run damblocks-mpdd mbs mbs-cron mutt news newsboat-num-cron newsboat-update-cron wobd xobd wttr reload"
-rules="99-damblocks-bluetooth.rules 99-damblocks-usb-audio.rules"
+rules="\
+    99-damblocks-bluetooth.rules \
+    99-damblocks-usb-audio.rules \
+    99-damblocks-battery.rules\
+"
 shells="bash zsh"
 
 update_damblocks() {
